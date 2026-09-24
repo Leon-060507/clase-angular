c4ffa14ab074ff3a7918e78d15c3c7e3697b1c77
LowenKoloss <U24248023@utp.edu.pe>
2026-09-24 14:59:08 +0000

    cambios

--- mini-lms/src/app/app.ts
+++ mini-lms/src/app/app.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 interface Estudiante {
 nombre: string;
 creditos: number;
+edad: number;
 }
 @Component({
 selector: 'app-root',
@@ -15,9 +16,9 @@ export class App {
 nombre = 'Juan';
 titulo = 'Mini-LMS · Lista de estudiantes';
 estudiantes: Estudiante[] = [
-{ nombre: 'María Torres', creditos: 18 },
-{ nombre: 'Luis Pérez', creditos: 8 },
-{ nombre: 'Ana Ruiz', creditos: 14 }
+{ nombre: 'María Torres', creditos: 18, edad: 16 },
+{ nombre: 'Luis Pérez', creditos: 8, edad: 19 },
+{ nombre: 'Ana Ruiz', creditos: 14, edad: 22 }
 ];
 // Misma regla de matrícula de la Unidad 1, ahora como método del componente.
 estado(creditos: number): string {
@@ -28,4 +29,13 @@ return 'Matriculado';
 }
 return 'Pendiente';
 }
+MayorEdad(edad: number): string{
+  if(edad<1){
+    return 'edad incorrecta';
+  }else if(edad>=18){
+    return 'Mayor de Edad';
+  }
+  return 'Menor de Edad';
 }
+}
+
